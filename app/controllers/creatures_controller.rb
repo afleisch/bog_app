b980ae529f14b343956a00489dbde67f4632f1b1
afleisch <afleisch@gmail.com>
2014-01-21 18:54:43 -0800

    added #update

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -26,18 +26,19 @@ class CreaturesController < ApplicationController
 
   def edit
   	creature_id = params[:id]
-  	Creature.find(creature_id)
+  	@creature = Creature.find(creature_id)
   	render :edit
   end
 
+  def update
+  	creature_id = params[:id]
+  	creature = Creature.find(creature_id)
 
+  	updated_info = params.require(:creature).permit(:name, :description)
+  	creature.update_attributes(updated_info)
 
-  
+  	redirect_to "/creatures/#{creature.id}"
   end
-
-
-
-
 
 
 
